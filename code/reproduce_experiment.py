10c5f231f7685c329c610671aa0c2f77f17c7a3c
taniya-das <taniyatd30@gmail.com>
2023-08-21 18:38:15 +0200

    modify reprodue code for REANA

--- code/reproduce_experiment.py
+++ code/reproduce_experiment.py
@@ -1,44 +1,72 @@
+# -*- coding: utf-8 -*-
+
+"""REANA-Demo-Run OpenML experiment"""
+
+import os
 import openml
 import warnings
-import numpy as np
-
-openml.config.start_using_configuration_for_example()
-warnings.simplefilter(action='ignore', category=FutureWarning)
+import errno
 
 
-MAX_EXP = 10
-runs = openml.runs.list_runs(size = MAX_EXP)
+def run_exp(outputfile="results/output.txt"):
+    
+    
+    # ensure output directory exists:
+    # influenced by http://stackoverflow.com/a/12517490
+    if not os.path.exists(os.path.dirname(outputfile)):
+        try:
+            os.makedirs(os.path.dirname(outputfile))
+        except OSError as exc:  # guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+            
+    # write greetings:
+    with open(outputfile, "at") as f:
+        
+        openml.config.start_using_configuration_for_example()
+        warnings.simplefilter(action='ignore', category=FutureWarning)
+        
+        MAX_EXP = 2
+        runs = openml.runs.list_runs(size = MAX_EXP)
 
-for key, run_ in runs.items():
-    run_id = run_['run_id']
-    
-    # breakpoint()
-    # Run original
-    run_original = openml.runs.get_run(run_id)
-    
-    print('Experiment number : ', key)
-    
-    # Download the flow and solve the same task again.
-    setup_id = run_['setup_id']
-    model_duplicate = openml.setups.initialize_model(setup_id)
-    
-    #flow = openml.flows.get_flow(run_['flow_id'])
-    task = openml.tasks.get_task(run_['task_id'])
+        for key, run_ in runs.items():
+            run_id = run_['run_id']
+        
+            # Run original
+            run_original = openml.runs.get_run(run_id)
+            
+            print('Running Experiment number ', key)
+            
+            # Download the flow and solve the same task again.
+            setup_id = run_['setup_id']
+            model_duplicate = openml.setups.initialize_model(setup_id)
+            
+            #flow = openml.flows.get_flow(run_['flow_id'])
+            task = openml.tasks.get_task(run_['task_id'])
 
-    run_duplicate = openml.runs.run_model_on_task(model_duplicate, task = task, avoid_duplicate_runs=False)
+            run_duplicate = openml.runs.run_model_on_task(model_duplicate, task = task, avoid_duplicate_runs=False)
+            predictions = run_duplicate.data_content
+            
+            entry = f"\n Experiment number {key}: {run_duplicate}\n"
+            f.write("{}".format(entry))
+            # f.write("{}".format(predictions))
+            f.flush()
+            
+        openml.config.stop_using_configuration_for_example()
+    f.close()
     
-    # results already published. No need to publish again  
-    # myrun = run_duplicate.publish()
+    return 
+
+if __name__ == '__main__':
     
-    print(run_duplicate)
-    print(f"Run was uploaded to {run_original.openml_url}")
-    
-    
-    # # the run has stored all predictions in the field data content
-    # np.testing.assert_array_equal(run_original.data_content, run_duplicate.data_content)
-    # print("Predictons: ", run_duplicate.data_content)
+    run_exp(outputfile="results/output.txt")
     
     
-openml.config.stop_using_configuration_for_example()
+
+
+   
+    
+    
+
     
     
